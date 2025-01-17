31123c1abec4198872553d12df85758cdc5c5c2c
Stephanie Duncan <smduncanarq@gmail.com>
2025-01-16 22:27:02 -0500

    Level Update

---
+++ Assets/Scripts/PlantGrowSys.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlantaBase : MonoBehaviour
+{
+    [SerializeField] protected float growingtime;
+    [SerializeField] protected Sprite[] phases; //Sprites for each growth phase
+    
+    protected int currentPhase = 0;  // Current phase of the plant (starts at 0, seed)
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(Growing());
+    }
+
+    
+    private IEnumerator Growing()
+    {
+        while (currentPhase < phases.Length - 1)  // While it is not in the harvest phase
+        {
+            yield return new WaitForSeconds(growingtime); // Wait 7 seconds between phases
+            SwitchPhase();  // Switch to the next phase
+        }
+    }
+
+    // Switch to the next phase (growth phase)
+    private void SwitchPhase()
+    {
+        currentPhase++;  // Advance to the next phase
+        spriteRenderer.sprite = phases[currentPhase];  // Change the sprite to the next one in the array
+    }
+
+    // Method to harvest the plant
+    public void Harvest()
+    {
+        if (currentPhase == phases.Length - 1)  // If it is in the harvest phase
+        {
+            spriteRenderer.sprite = phases[0];  // Reset the plant to the seed phase
+            currentPhase = 0;  // Restart to seed phase
+            Invoke("ResetGrowth", 5f);  // Wait 5 seconds and start again
+        }
+    }
+
+    // Restart the plant's growth cycle
+    private void ResetGrowth()
+    {
+        StartCoroutine(Growing());  
+    }
+
+    // Method to know if the plant is ready to be harvested
+    public bool ItsHarvested()
+    {
+        return currentPhase == phases.Length - 1;  // If it is in the harvest phase
+    }
+}
--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    [SerializeField] private float movSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        rb.velocity = new Vector2(speedX, speedY);
     }
 }
